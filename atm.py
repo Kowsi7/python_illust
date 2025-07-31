62bb49b4942f99249fbcf3845a651d39ee41ed89
Kowsi7 <kowsikaanandhan@gmail.com>
2025-07-31 15:56:46 +0530

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,13 @@
+print("select the services you want:")
+print('1.withdraw')
+print('2.deposit\n3.savings\n4.balance')
+PIN=1234
+service=int(input('kindly select:'))
+if service==1:
+    print('withdraw')
+amount=int(input('enter a amount:'))
+while
+if pin==PIN:
+  print('enter your pin')
+ print('collect your amount',amount)
+ 
