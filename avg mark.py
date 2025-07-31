cf1220abe245ddc9d6be1069e405c972f013ba60
Kowsi7 <kowsikaanandhan@gmail.com>
2025-07-31 15:57:39 +0530

    Create avg mark.py

---
+++ avg mark.py
@@ -0,0 +1,9 @@
+num=int (input("Enter the mark:"))
+if num>=90:
+    print(" grade A")
+elif num>=80  or  num<90:
+    print(" grade B")
+elif num>=50  or  num<=90:
+    print("grade C")
+else:    
+    print("grade D")
